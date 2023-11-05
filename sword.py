50f6dee860b486c0b293983f77f610c9c6f6ea79
wonju <jwwon00@naver.com>
2023-11-05 21:03:34 +0900

    검 이미지 추가
    아직 출력 미완성

--- sword.py
+++ sword.py
@@ -1,22 +1,18 @@
 from pico2d import *
 import game_world
 
-class Ball:
+class Sword:
     image = None
-
     def __init__(self, x = 400, y = 300, velocity = 1):
-        if Ball.image == None:
-            Ball.image = load_image('ball21x21.png')
+        if Sword.image == None:
+            Sword.image = load_image('sword_05.png')
         self.x, self.y, self.velocity = x, y, velocity
 
     def draw(self):
         self.image.draw(self.x, self.y)
 
     def update(self):
-        self.x += self.velocity
-
-        if self.x < 25 or self.x > 800 - 25:
-            game_world.remove_object(self)
+        pass
 
 class BigBall:
     image = None
