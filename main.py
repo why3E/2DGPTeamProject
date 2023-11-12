0529065d8a79439c64431f7b29362266bb9d281e
wonju <jwwon00@naver.com>
2023-11-12 21:48:33 +0900

    발표자료 정리

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pico2d import open_canvas, delay, close_canvas
 import game_framework
 #import logo_mode as start_mode
-#import title_mode as start_mode
-import play_mode as start_mode
+import title_mode as start_mode
+#import play_mode as start_mode
 
 open_canvas(800, 800)
 game_framework.run(start_mode)
