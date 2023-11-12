e9e4b89408e4ed1b5cb16eb15e43dec4d45ff183
wonju <jwwon00@naver.com>
2023-11-12 11:57:40 +0900

    몬스터 이동 구현완료

--- monster.py
+++ monster.py
@@ -8,7 +8,7 @@ import game_world
 
 # zombie Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 10.0  # Km / Hour
+RUN_SPEED_KMPH = 5.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -16,53 +16,53 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 # zombie Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 10.0
+FRAMES_PER_ACTION = 4.0
 
-animation_names = ['Walk']
 
-class Zombie:
-    images = None
+class Ghost:
+    image = None
 
     def load_images(self):
-        if Zombie.images == None:
-            Zombie.images = {}
-            for name in animation_names:
-                Zombie.images[name] = [load_image("./zombie/"+ name + " (%d)" % i + ".png") for i in range(1, 11)]
+        if Ghost.image == None:
+            Ghost.image =load_image('source/ghost_AT.png')
 
-    def __init__(self):
-        self.x, self.y = random.randint(400, 500), 150
+    def __init__(self, main_character):
+        self.x, self.y = main_character.x + random.randint(200, 300), main_character.y + random.randint(200, 300)
+        self.main_character = main_character
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
-        self.size = 200
+        self.dir2 = random.choice([-1,1])
+        self.size = 32
 
 
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
-        self.x += RUN_SPEED_PPS * self.dir * game_framework.frame_time
-        if self.x > 500:
-            self.dir = -1
-        elif self.x < 400:
-            self.dir = 1
-        self.x = clamp(400, self.x, 500)
+
+        self.dir = self.main_character.x - self.x
+        self.dir2 = self.main_character.y - self.y
+
+        distance = (self.dir ** 2 + self.dir2 ** 2) ** 0.5
+
+        if distance != 0:
+            self.dir /= distance
+            self.dir2 /= distance
+
+        if self.dir != 0 and self.dir2 != 0:
+            self.x += RUN_SPEED_PPS * self.dir * game_framework.frame_time*0.5
+            self.y += RUN_SPEED_PPS * self.dir2 * game_framework.frame_time*0.5
+        else:
+            self.x += RUN_SPEED_PPS * self.dir * game_framework.frame_time
+            self.y += RUN_SPEED_PPS * self.dir2 * game_framework.frame_time
 
         pass
 
 
     def draw(self):
-        if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.size, self.size)
+        if self.dir >= 0:
+            self.image.clip_composite_draw(int(self.frame) * 32, 0, 32, 32, 0, 'h', self.x, self.y, 32, 32)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.size, self.size)
-        draw_rectangle(*self.get_bb())
+            self.image.clip_draw(int(self.frame) * 32, 0, 32, 32, self.x, self.y,32,32)
 
-    def handle_collision(self, group, other):
-        if group == 'zombie:ball':
-            self.size -= 100
-            self.y -= 50
-            if(self.size <=0):
-                game_world.remove_object(self)
-
-    def get_bb(self):
-        return self.x - self.size/4 , self.y - self.size/2, self.x + self.size/4, self.y + self.size/4
-
+# 피격판정을 받았을떄 풍선을 만든다(스킬 3개니까 각기 다른 풍선 3개) 하고 만약 해당하는 피격 종류를 받으면 풍선을 터트리고
+# 시간을 잰 다음 다시 부풀어 오르게 하자)
